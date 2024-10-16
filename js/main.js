3c19489b2ce6c158a405cacedbf0e0fac74a36de
“Patricio <dalessandropatricio@gmail.com>
2024-10-15 21:07:58 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,136 +1,110 @@
 const usuario = "patricio";
-const contraseña = "patito123";
+const contrasena = "patito123";
 
-let codVestidoNegro = 1
-let precioVestidoNegro = 30500 ;
+// Productos y precios
+let codVestidoNegro = 1;
+let precioVestidoNegro = 30500;
 
-let  codRemeraGris = 2 ;
-let  precioRemeraGris = 15000 ;
+let codRemeraGris = 2;
+let precioRemeraGris = 15000;
 
-let  codJeanBlue = 3 ;
-let  precioJeanBlue  = 55000 ;
+let codJeanBlue = 3;
+let precioJeanBlue = 55000;
 
-let  codSweaterGris = 4 ;
-let  precioSweaterGris = 50000 ;
+let codSweaterGris = 4;
+let precioSweaterGris = 50000;
 
-alert ("Bienvenid@ al carrito de compras de Lutto Clothing. \nSi desea comprar debe loguearse a continuación");
+alert("Bienvenido al carrito de compras de Lutto Clothing. \nSi desea comprar debe loguearse a continuacion");
 
-let user = prompt("Ingrese su usuario");
-let pass = prompt("Ingrese su contraseña");
+let usuarioIngresado = prompt("Ingrese su usuario");
+let contrasenaIngresada = prompt("Ingrese su contrasena");
 
-if (user != "" && pass != "" ){
-
-
-    if ((user === usuario ) && (pass === contraseña)){
-
-        alert("Hola "+usuario+".\nPara comprar nuestros productos deberás ingresar el código que corresponda de la siguiente lista ");
-
-        let sumaCarrito = 0 ;
-        let listaProductos = "" ;
-
-        let codProd = parseInt(prompt("1: Vestido Negro Acanalado = $30500 \n 2: Remera Gris Básica = 15000 \n 3: Jean Blue Classic = $55000 \n 4: Sweater Gris 50000 \n Ingrese el codigo de un producto"));
-
-        console.log(codProd);
-
-        if (!isNaN(codProd)){
-
-            if(codProd === codVestidoNegro){
-
-                listaProductos += "Vestido Negro Acanalado $" + precioVestidoNegro + "\n";
-                sumaCarrito += precioVestidoNegro ;
-            }else if (codProd === codRemeraGris){
-
-                listaProductos += "Remera Gris Básica $" + precioRemeraGris + "\n";
-                sumaCarrito += precioRemeraGris ;
-
-            }else if (codProd === codJeanBlue){
-
-                listaProductos += "Jean Blue Classic" + precioJeanblue + "\n";
-                sumaCarrito += precioJeanblue ;
-
-            }else if (codProd === codSweaterGris){
-
-                listaProductos += "Sweater Gris" + precioSweaterGris + "\n";
-                sumaCarrito += precioSweaterGris ;
-            }else {
-
-                console.log("codigo invalido");
-            }
-
-            
-
-
-
-        }else {
-
-            alert("Debes ingresar un numero");
-
-        }
-
-        codProd = parseInt(prompt("Ingrese el codigo de un segundo producto"));
-
-        console.log(codProd);
-
-        if (!isNaN(codProd)){
-
-            if(codProd === codVestidoNegro){
-
-                listaProductos += "Vestido Negro Acanalado $" + precioVestidoNegro + "\n";
-                sumaCarrito += precioVestidoNegro ;
-                
-            }else if (codProd === codRemeraGris){
-
-                listaProductos += "Remera Gris Básica $" + precioRemeraGris + "\n";
-                sumaCarrito += precioRemeraGris ;
-
-            }else if (codProd === codJeanBlue){
-
-                listaProductos += "Jean Blue Classic" + precioJeanBlue + "\n";
-                sumaCarrito += precioJeanBlue ;
-
-            }else if (codProd === codSweaterGris){
-
-                listaProductos += "Sweater Gris" + precioSweaterGris + "\n";
-                sumaCarrito += precioSweaterGris ;
-            }else {
-
-                console.log("codigo invalido");
-            }
-
-
-            if (sumaCarrito > 0) {
-
-                alert("Usted compro:\n"+listaProductos+"\n"+"La suma total de la compra es: $"+sumaCarrito);
-
-            }else {
-
-                alert("No fue posible realizar la compra");
-            }
-            
-
-
-
-        }else {
-
-            alert("Debes ingresar un numero");
-
-        }
-
-
-
-    }else{
-
-        alert("Usuario y/o contraseña incorrectas.Recarga la pagina!!");
-
-    }
-
-
-
-
-}else{
-
-
-    alert("Tenes que ingresar todos los datos. Recarga la pagina! (para esto puedes utlizar la tecla F5)");
-
+// Funcion para validar el login
+function validarLogin(user, pass) {
+    return (user === usuario && pass === contrasena);
 }
 
+// Funcion para mostrar el menu de productos
+function mostrarMenu() {
+    return parseInt(prompt(
+        "1: Vestido Negro Acanalado = $30500 \n" +
+        "2: Remera Gris Basica = $15000 \n" +
+        "3: Jean Blue Classic = $55000 \n" +
+        "4: Sweater Gris = $50000 \n" +
+        "Ingrese el codigo de un producto"));
+}
+
+// Funcion para procesar el codigo de producto
+function procesarProducto(codigoProducto) {
+    let producto = "";
+    let precio = 0;
+
+    switch (codigoProducto) {
+        case codVestidoNegro:
+            producto = "Vestido Negro Acanalado";
+            precio = precioVestidoNegro;
+            break;
+        case codRemeraGris:
+            producto = "Remera Gris Basica";
+            precio = precioRemeraGris;
+            break;
+        case codJeanBlue:
+            producto = "Jean Blue Classic";
+            precio = precioJeanBlue;
+            break;
+        case codSweaterGris:
+            producto = "Sweater Gris";
+            precio = precioSweaterGris;
+            break;
+        default:
+            alert("Codigo de producto invalido.");
+            return; // Retorna sin hacer nada si hay un código inválido
+    }
+
+    // Muestra el producto y precio en un mensaje
+    alert("Producto seleccionado: " + producto + "\nPrecio: $" + precio);
+    return precio; // Retorna el precio para que pueda sumarse al carrito
+}
+
+// Funcion para gestionar la compra
+function realizarCompra() {
+    let sumaCarrito = 0;
+    let listaProductos = "";
+    let seguirComprando = true;
+
+    while (seguirComprando) {
+        let codProd = mostrarMenu();
+
+        if (!isNaN(codProd)) {
+            let precio = procesarProducto(codProd);
+            
+            // Si el precio es válido, añadimos a la lista
+            if (precio > 0) {
+                listaProductos += "Producto: " + codProd + " - Precio: $" + precio + "\n"; 
+                sumaCarrito += precio;
+            }
+        } else {
+            alert("Debes ingresar un numero valido.");
+        }
+
+        seguirComprando = confirm("¿Desea seguir comprando?");
+    }
+
+    if (sumaCarrito > 0) {
+        alert("Usted compro:\n" + listaProductos + "\nLa suma total de la compra es: $" + sumaCarrito);
+    } else {
+        alert("No se realizaron compras.");
+    }
+}
+
+// Validacion de usuario y contrasena
+if (usuarioIngresado !== "" && contrasenaIngresada !== "") {
+    if (validarLogin(usuarioIngresado, contrasenaIngresada)) {
+        alert("Hola " + usuario + ".\nPara comprar nuestros productos deberas ingresar el codigo que corresponda de la siguiente lista.");
+        realizarCompra();
+    } else {
+        alert("Usuario y/o contrasena incorrectas. Recarga la pagina.");
+    }
+} else {
+    alert("Debes ingresar todos los datos. Recarga la pagina.");
+}
